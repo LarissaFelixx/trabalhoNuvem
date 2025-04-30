b859a38a21f7d4600fa926ba3fcf7675e600bf47
Larissa <larissafelix@edu.unifor.brgit config --global --listgit config --global user.email>
2025-04-29 22:24:14 -0300

    commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express(); // iniciando o express
 
 // criando a rota inicial
 app.get("/", function (req, res) {
-  res.send("<h1>Seja bem-vindo ao meu site atualizado!</h1>");
+  res.send("<h1>Seja bem-vindo ao meu site!</h1>");
 });
 
 // rota do cadastro de produtos
